2f223ba784a9595de5ce03eb946d2c5f1b66af19
Artha Four <Arumifathina@gmail.com>
2025-04-14 05:56:13 +0700

    fix: add alias @ for src path

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },typescript:{
     ignoreBuildErrors: true,
-  }
+  },webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "@": path.resolve(__dirname, "src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
